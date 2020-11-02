f5c05401ca76f58ed9d10bc0d78622d39d384d45
王世安 <984292420@qq.com>
2020-11-02 23:32:34 +0800

    feat: 修改构建

--- build.ts
+++ build.ts
@@ -2,5 +2,6 @@ require('esbuild').build({
   entryPoints: ['main.tsx'],
   jsxFactory: 'createElement',
   bundle: true,
+  minify: true,
   outfile: 'dist/main.js',
 }).catch(() => process.exit(1))
